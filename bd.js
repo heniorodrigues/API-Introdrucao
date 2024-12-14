7a48f1279d173309fa07ad3f8c0fd73a320ce7cf
heniorodrigues <hrrs@aluno.ifnmg.edu.br>
2024-12-14 23:52:46 +0000

    atualizado 2

--- bd.js
+++ bd.js
@@ -25,6 +25,7 @@ async function selectUsuario(id) {
   return res.rows;
 }
 
+//bd.js
 async function insertUsuario(data) {
   const client = await connect();
   const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
@@ -33,19 +34,5 @@ async function insertUsuario(data) {
   client.release();
 }
 
-async function deleteUsuario(id) {
-  const client = await connect();
-  const query = "DELETE FROM usuario WHERE id = $1";
-  await client.query(query, [id]);
-  client.release();
-}
-
-async function updateUsuario(id, data) {
-  const client = await connect();
-  const query = "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
-  const usuario = [data.nome, data.email, data.senha, id];
-  await client.query(query, usuario);
-  client.release();
-}
-
-export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario };
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 import express from "express";
 import dotenv from "dotenv"; 
-import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";   
-
+import { selectUsuarios, selectUsuario, insertUsuario,} from "./bd.js";   
 dotenv.config();
 
 const app = express();          
 const port = 3000;
 
+//index.js
 app.use(express.json());
 
 app.get("/", (req, res) => {      
@@ -49,34 +49,7 @@ app.post("/usuario", async (req, res) => {
   }
 });
 
-app.delete("/usuario/:id", async (req, res) => {
-  console.log("Rota DELETE /usuario/# solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) {
-      await deleteUsuario(req.params.id);
-      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-app.put("/usuario/:id", async (req, res) => {
-  console.log("Rota PUT /usuario/# solicitada");
-  try {
-    const id = req.params.id;
-    const usuario = await selectUsuario(id);
-    if (usuario.length > 0) {
-      await updateUsuario(id, req.body);
-      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    console.log(error);
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
 app.listen(port, () => {       
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
